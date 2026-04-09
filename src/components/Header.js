1c3a07d2eae94976f0e5997835c7230a7977e571
sagar dhakal <shagar.dhakal77@gmail.com>
2026-04-10 00:17:51 +0300

    make it responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,39 @@
 // src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 
 // This is the Header component, it will render the navigation bar
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="header glass-nav">
-      <nav className="nav">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
+      <div className="header-container">
+        {/* Hamburger Icon */}
+        <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        
+        <nav className={`nav ${isMenuOpen ? 'nav-active' : ''}`}>
+          <ul>
+            <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+            <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+            <li><Link to="/projects" onClick={closeMenu}>Projects</Link></li>
+            <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
